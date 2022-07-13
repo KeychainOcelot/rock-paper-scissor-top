a2d9a12146e518a011b1ee914d778bd4072778f2
Shunal Doke <mysticshunal@gmail.com>
2022-07-13 07:22:51 +0530

    rewrote game loop. scoring needs rewrite.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
-//currently goes into infinite loop
-
-const RPS = ["rock", "paper", "scissor"];
+const RPS = ["rock", "paper", "scissor"]; //might delete this. not really used anywhere
 let playerScore = 0;
 let computerScore = 0;
 
 function playerPlay() {
     //get player input
     let playerSelection = 0;
-    const choice = prompt("Please enter 'rock', 'paper', or 'scissor'");
-    for(let i = 0; i < 3; i++) {
-        if(choice == RPS[i]) {
+    let choice = prompt("Please enter '0' for rock, '1' for paper, or '2' for scissor");
+    for (let i = 0; i < 3; i++) {
+        if (choice == RPS[i]) {
             playerSelection = i;
             break;
         }
@@ -20,50 +18,56 @@ function playerPlay() {
 function computerPlay() {
     //randomiser to make computer pick from RPS array
     let computerSelection = 0;
-    computerSelection = Math.floor(Math.random() * RPS.length);
+    computerSelection = random(0, 3);
     return computerSelection;
 }
 
 function gameLoop(playerSelection, computerSelection) {
     let playerWin = false;
-    if(playerSelection == RPS[0] && computerSelection == RPS[1]) {
+    if (playerSelection == computerSelection) {
         playerWin = false;
-    } else if (playerSelection == RPS[0] && computerSelection == RPS[2]) {
+    } else if (playerSelection == 0 && computerSelection == 1) {
+        playerWin = false;
+    } else if (playerSelection == 0 && computerSelection == 2) {
         playerWin = true;
-    } else if (playerSelection == RPS[1] && computerSelection == RPS[0]) {
+    } else if (playerSelection == 1 && computerSelection == 0) {
         playerWin = true;
-    } else if (playerSelection == RPS[1] && computerSelection == RPS[2]) {
+    } else if (playerSelection == 1 && computerSelection == 2) {
         playerWin = false;
-    } else if (playerSelection == RPS[2] && computerSelection == RPS[0]) {
+    } else if (playerSelection == 2 && computerSelection == 0) {
         playerWin = false;
-    } else if (playerSelection == RPS[2] && computerSelection == RPS[1]) {
+    } else if (playerSelection == 2 && computerSelection == 1) {
         playerWin = true;
-    } else if (playerSelection == computerSelection) {
-        playerWin = false;
+    }
+
+    if (playerWin == true) {
+        window.alert("You win!");
+        window.alert(`Your points: ${playerScore}
+        Computer's points: ${computerScore}`);
     }
     return playerWin;
 }
 
-function getScore() {
-    let thisTurn = false;
-    playerSelection = playerPlay;
-    computerSelection = computerPlay;
-    thisTurn = gameLoop(playerSelection, computerSelection);
-    if(thisTurn == false) {
-        computerScore += 1;
-    } else if(thisTurn == true) {
-        playerScore += 1;
-    }
-}
+//scoring has to be completely rewritten
+
+// function getScore() {
+//     let thisTurn = false;
+//     playerSelection = playerPlay;
+//     computerSelection = computerPlay;
+//     thisTurn = gameLoop(playerSelection, computerSelection);
+//     if (thisTurn == false) {
+//         computerScore += 1;
+//     } else if (thisTurn == true) {
+//         playerScore += 1;
+//     }
+// }
 
 function game() {
     const letsPlay = prompt("Press enter to play Rock, Paper, Scissor");
-    while(playerScore + computerScore != 5) {
-        const player = playerPlay();
-        const computer = computerPlay();
-        gameLoop(playerPlay(), computerPlay()); //this should work
-        getScore();
-    }
+    const player = playerPlay();
+    const computer = computerPlay();
+    gameLoop(playerPlay(), computerPlay()); //this should work
+    getScore();
 }
 
 game();
