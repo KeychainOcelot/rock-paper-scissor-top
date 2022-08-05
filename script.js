50c6ef766606404de4cf5bdc8943a84e08dbe44d
Shunal Doke <mysticshunal@gmail.com>
2022-08-06 03:20:06 +0530

    Re-doing the entirety of rock paper scissors Making use of newer things I learned

---
+++ script.js
@@ -0,0 +1,38 @@
+//a complete do-over of the rock paper scissor Odin Project assignment
+
+const intro = function() {
+    window.alert(`Welcome to Rock, paper, scissor!
+        Click okay to start!`);
+
+}
+
+
+const turn = function() {
+    
+}
+
+const gameLoop = function() {
+    let playerScore = 0, computerScore = 0;
+    
+    //decide turn
+    //player turn
+    //computer turn
+    //decide winner
+    //keep going until playerScore || computerScore === 5
+}
+
+const getWinner = (playerScore = 0, computerScore = 0) => {
+    let winner = false;
+    if(playerScore === 5) {
+        return winner = true;
+    } else {
+        return winner = false;
+    }
+}
+
+
+const game = function() {
+    intro();
+}
+
+game();
